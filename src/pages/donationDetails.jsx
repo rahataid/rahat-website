ee9f20930c019718d7f2a0f8d0718ba8f8c0c0d3
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-05-09 15:54:38 +0545

    login page for donor and beneficiary

---
+++ src/pages/donationDetails.jsx
@@ -0,0 +1,40 @@
+import SEO from "@components/seo";
+import Footer from "@layout/footer/footer-01";
+import Header from "@layout/header/header-01";
+import Wrapper from "@layout/wrapper";
+import TabContainer from "react-bootstrap/TabContainer";
+import TabContent from "react-bootstrap/TabContent";
+import TabPane from "react-bootstrap/TabPane";
+import EditProfileImage from "../containers/edit-profile/edit-profile-image";
+
+
+const EditProfile = () => (
+    <Wrapper>
+        <SEO pageTitle="Donation Details" />
+        <Header />
+        <div className="edit-profile-area rn-section-gapTop">
+            <div className="container">
+                <div className="row plr--70 padding-control-edit-wrapper pl_md--0 pr_md--0 pl_sm--0 pr_sm--0">
+                    <div className="col-12 d-flex justify-content-between mb--30 align-items-center">
+                        <h4 className="title-left">Donation Details</h4>
+                    </div>
+                </div>
+                <TabContainer defaultActiveKey="nav-home">
+                    <div className="row plr--70 padding-control-edit-wrapper pl_md--0 pr_md--0 pl_sm--0 pr_sm--0">
+                        <div className="col-lg-12 col-md-12 col-sm-12 mt_sm--30">
+                            <TabContent className="tab-content-edit-wrapepr">
+                                <TabPane eventKey="nav-home">
+                                    <EditProfileImage />
+                                </TabPane>
+                            </TabContent>
+                        </div>
+                    </div>
+                </TabContainer>
+            </div>
+        </div>
+        <Footer />
+    </Wrapper>
+);
+
+export default EditProfile;
+
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,9 @@
-import SEO from "@components/seo";
-import Wrapper from "@layout/wrapper";
-import Header from "@layout/header/header-01";
-import Footer from "@layout/footer/footer-01";
 import Breadcrumb from "@components/breadcrumb";
+import SEO from "@components/seo";
 import LoginArea from "@containers/login";
+import Footer from "@layout/footer/footer-01";
+import Header from "@layout/header/header-01";
+import Wrapper from "@layout/wrapper";
 
 export async function getStaticProps() {
     return { props: { className: "template-color-1" } };
@@ -14,7 +14,7 @@ const Login = () => (
         <SEO pageTitle="Log In" />
         <Header />
         <main id="main-content">
-            <Breadcrumb pageTitle="Nuron Login" currentPage="Nuron Login" />
+            <Breadcrumb pageTitle="Login" currentPage="Login" />
             <LoginArea />
         </main>
         <Footer />
