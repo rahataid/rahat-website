c2f9d9eeea102631cc69293f6e0fc551382def71
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-06-01 15:09:35 +0545

    logo redirect to rahat.io

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import Anchor from "@ui/anchor";
-import PropTypes from "prop-types";
 import clsx from "clsx";
+import Image from "next/image";
+import PropTypes from "prop-types";
 
 const Logo = ({ className, logo }) => (
     <div className={clsx("logo-thumbnail logo-custom-css", className)}>
         {logo?.[0]?.src && (
-            <Anchor className="logo-light" path="/">
+            <Anchor className="logo-light" path="https://rahat.io/">
                 <Image
                     src={logo[0].src}
                     alt={logo[0]?.alt || "nft-logo"}
@@ -16,7 +16,7 @@ const Logo = ({ className, logo }) => (
             </Anchor>
         )}
         {logo?.[1]?.src && (
-            <Anchor className="logo-dark" path="/">
+            <Anchor className="logo-dark" path="https://rahat.io/">
                 <Image
                     src={logo[1].src}
                     alt={logo[1]?.alt || "nft-logo"}
