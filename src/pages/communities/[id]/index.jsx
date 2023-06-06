860c4e08f699e4757d707da486ff79bbc7b2db97
Raghav Kattel <arg.ahv7@gmail.com>
2023-06-06 09:47:10 +0545

    clean

--- src/pages/communities/[id]/index.jsx
+++ src/pages/communities/[id]/index.jsx
@@ -4,23 +4,16 @@ import CommunityIntro from "@containers/community-details/communityIntro";
 import Footer from "@layout/footer/footer-01";
 import Header from "@layout/header/header-01";
 import Wrapper from "@layout/wrapper";
-import {
-    getCommunityDetails,
-    getCommunityProjects,
-} from "@redux/slices/community";
+import { getCommunityDetails } from "@redux/slices/community";
 import { wrapper } from "@redux/store";
 
-const Author = ({ community, id, projects }) => (
+const Author = ({ community, id }) => (
     <Wrapper>
         <SEO pageTitle="Author" />
         <Header />
         <main id="main-content">
             <CommunityIntro community={community} />
-            <CommunityDetails
-                community={community}
-                id={id}
-                projects={projects}
-            />
+            <CommunityDetails community={community} />
         </main>
         <Footer />
     </Wrapper>
@@ -30,9 +23,7 @@ export const getServerSideProps = wrapper.getServerSideProps(
     (store) =>
         async ({ query }) => {
             await store.dispatch(getCommunityDetails(query?.id));
-            await store.dispatch(getCommunityProjects(query?.id));
             const community = store.getState().community.community;
-            const projects = store.getState().community.projects;
             const serializedError = store.getState().community.error;
             return {
                 props: {
