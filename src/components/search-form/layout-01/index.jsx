c5c473ab51ced4e1ede36a725ad76c8af92dfa55
Raghav Kattel <arg.ahv7@gmail.com>
2023-06-09 11:53:05 +0545

    search refactor

--- src/components/search-form/layout-01/index.jsx
+++ src/components/search-form/layout-01/index.jsx
@@ -1,59 +1,89 @@
 import NiceSelect from "@ui/nice-select";
-import { usePathname, useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
+
 const SearchForm = ({ categories, countries = [] }) => {
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    let countryQuery;
-    if (countries[0]) {
-        countryQuery = countries?.reduce((country) => {
-            if (country?.value === searchParams.get("country")) return country;
-        });
-    }
-    const categoryQuery = categories?.reduce((category) => {
-        if (category?.value == searchParams.get("category")) return category;
-    });
-    console.log({ categoryQuery });
-    const [search, setSearch] = useState(searchParams.get("search"));
-    const [country, setCountry] = useState(countryQuery);
-    const [category, setCategory] = useState(categoryQuery);
+    const [search, setSearch] = useState(searchParams.get("name"));
+    const [country, setCountry] = useState(
+        countries.find(
+            (country) => country.value === searchParams.get("country")
+        )
+    );
+
+    const [category, setCategory] = useState(
+        categories.find(
+            (category) => category.value === searchParams.get("category")
+        )
+    );
 
     const { push } = useRouter();
+
     const handleSearchChange = (e) => {
-        setSearch(e.target.value);
+        const { value, name } = e.target;
+        setSearch(value);
+        push(
+            pathname +
+                "?" +
+                createQueryString({
+                    [name]: value,
+                    category: category?.value,
+                    country: country?.value,
+                })
+        );
     };
 
-    const handleSearch = () => {
-        push(pathname + "?" + createQueryString("search", search));
-    };
+    // const handleSearch = () => {};
 
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            // 👇 Get input value
-            handleSearch();
+    // const handleKeyDown = (event) => {
+    //     if (event.key === "Enter") {
+    //         handleSearch(event);
+    //     }
+    // };
+
+    const handleOptionChange = (name, data) => {
+        if (!name || !data) return;
+
+        const updatedParams = {
+            ...Object.fromEntries(searchParams.entries()),
+            [name]: data.value,
+        };
+        const queryString = createQueryString(updatedParams);
+        push(pathname + "?" + queryString);
+
+        if (name === "category") {
+            setCategory(data);
+        } else if (name === "country") {
+            setCountry(data);
         }
     };
-    const handleCategory = (data) => {
-        push(pathname + "?" + createQueryString("category", data.value));
-        setCategory(data);
-    };
 
-    const handleCountry = (data) => {
-        push(pathname + "?" + createQueryString("country", data.value));
-        setCountry(data);
+    const clearFilter = () => {
+        setSearch("");
+        setCategory(null);
+        setCountry(null);
+        push(pathname);
     };
-
-    const clearFilter = () => {};
 
     const createQueryString = useCallback(
-        (name, value) => {
-            const params = new URLSearchParams(searchParams);
-            params.set(name, value);
+        (params) => {
+            const queryParams = Object.entries(params)
+                .filter(
+                    ([_, value]) =>
+                        value !== null && value !== undefined && value !== ""
+                )
+                .map(
+                    ([key, value]) =>
+                        `${encodeURIComponent(key)}=${encodeURIComponent(
+                            value
+                        )}`
+                )
+                .join("&");
 
-            return params.toString();
+            return queryParams === "" ? "" : `${queryParams}`;
         },
-        [searchParams]
+        [pathname]
     );
 
     return (
@@ -62,17 +92,17 @@ const SearchForm = ({ categories, countries = [] }) => {
                 <NiceSelect
                     options={categories}
                     placeholder="Filter By Category"
-                    name="like"
-                    onChange={handleCategory}
+                    name="category"
+                    onChange={(data) => handleOptionChange("category", data)}
                     defaultCurrent={category}
                 />
-            </div>{" "}
+            </div>
             <div className="filter-select-option">
                 <NiceSelect
                     options={countries}
                     placeholder="Filter By Country"
-                    name="like"
-                    onChange={handleCountry}
+                    name="country"
+                    onChange={(data) => handleOptionChange("country", data)}
                     defaultCurrent={country}
                 />
             </div>
@@ -81,17 +111,26 @@ const SearchForm = ({ categories, countries = [] }) => {
                     type="search"
                     placeholder="Search Here"
                     aria-label="Search"
+                    name="name"
                     value={search}
                     onChange={handleSearchChange}
-                    onKeyDown={handleSearch}
+                    // onKeyDown={handleKeyDown}
                 />
-
-                <div className="search-icon">
+                {/* <div className="search-icon">
                     <button type="button" onClick={handleSearch}>
                         <i className="feather-search" />
                     </button>
-                </div>
+                </div> */}
             </form>
+            <div className="reset-icon">
+                <button
+                    type="button"
+                    className="btn btn-sm btn-secondary"
+                    onClick={clearFilter}
+                >
+                    Clear
+                </button>
+            </div>
         </>
     );
 };
