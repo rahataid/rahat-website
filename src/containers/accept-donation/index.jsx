e804cfec4182e430838629d6ed77b7aeeb609513
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-06-23 13:51:33 +0545

    update on accept donation page

--- src/containers/accept-donation/index.jsx
+++ src/containers/accept-donation/index.jsx
@@ -1,3 +1,4 @@
+import Icon from '@components/iconify';
 import Button from "@ui/button";
 import clsx from "clsx";
 import PropTypes from "prop-types";
@@ -14,10 +15,10 @@ const PrivacyPolicyArea = ({ className, space, donations }) => (
             <div className="row mb_dec--50">
                 <div className="offset-lg-2 col-lg-8 ">
                     <div className="rn-accept-card">
-                        <h4>Accept donation from {"Rahat"} ?</h4>
-                        <p>{
-                            "lorem"
-                        }</p>
+                        <h4 className='mb--15'>Accept donation from {"Rahat"} ?</h4>
+                        <span className='mr--25'><Icon style={{ color: '#007bb6', fontSize: '24px', marginBottom: '2px' }} icon="solar:hand-money-linear" /> 50,000</span>
+                        <span><Icon style={{ color: '#007bb6', fontSize: '22px', marginBottom: '2px' }} icon="clarity:date-line" /> 20/11/2023</span>
+
                     </div>
                     <div className="rn-accept-card">
 
@@ -37,7 +38,7 @@ const PrivacyPolicyArea = ({ className, space, donations }) => (
                 </div>
             </div>
         </div>
-    </div>
+    </div >
 );
 
 PrivacyPolicyArea.propTypes = {
