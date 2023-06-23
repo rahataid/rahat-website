556fc8cc92326c3906e763c6d0d3c0236e413924
sarvesh-karki <sarvesh.karki@rumsan.net>
2023-06-23 11:45:54 +0545

    accept donation page

--- src/containers/accept-donation/index.jsx
+++ src/containers/accept-donation/index.jsx
@@ -13,18 +13,22 @@ const PrivacyPolicyArea = ({ className, space, donations }) => (
         <div className="container">
             <div className="row mb_dec--50">
                 <div className="offset-lg-2 col-lg-8 ">
-                    <div className="privacy-wrapper">
-                        <h4>Accept donation from {donations.donee.name} ?</h4>
-                        <p>{donations.donor.description}</p>
+                    <div className="rn-accept-card">
+                        <h4>Accept donation from {"Rahat"} ?</h4>
+                        <p>{
+                            "lorem"
+                        }</p>
+                    </div>
+                    <div className="rn-accept-card">
 
                         <h4>Terms and Conditions</h4>
-                        <p>{donations.terms}</p>
+                        <p>{"lorem"}</p>
                     </div>
                 </div>
             </div>
             <div className="row mt--50">
                 <div className="offset-lg-2 col-lg-8">
-                    <Button path="#" size="medium" className="mr--15 ml--25">
+                    <Button path="#" size="medium" className="mr--15">
                         Accept
                     </Button>
                     <Button path="#" color="primary-alta" size="medium">
