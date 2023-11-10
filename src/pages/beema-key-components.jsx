5f559309b284d81007c03770515fc89509696b72
Amrit Pandey <pandeyamrit714@gmail.com>
2023-11-10 15:01:22 +0545

    Rahat beema key component page

---
+++ src/pages/beema-key-components.jsx
@@ -0,0 +1,19 @@
+import SEO from "@components/seo";
+import LearnMorePage from "@containers/learn-more-page";
+import Footer from "@layout/footer";
+import Header from "@layout/header";
+import Wrapper from "@layout/wrapper";
+
+const RahatBeemaKeyComponentsDetailPage = () => {
+    return (
+        <Wrapper>
+            <SEO pageTitle="Rahat Beema Key Components" />
+            <Header />
+            <LearnMorePage />
+            <Footer />
+        </Wrapper>
+    );
+};
+
+export default RahatBeemaKeyComponentsDetailPage;
+
