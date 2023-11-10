9394dfbfb1549465cc07541925874723e50a1356
Amrit Pandey <pandeyamrit714@gmail.com>
2023-11-10 14:57:15 +0545

    rahat beema - achievements/badges

--- src/containers/rahat-beema-page/achievements-badges/index.jsx
+++ src/containers/rahat-beema-page/achievements-badges/index.jsx
@@ -11,7 +11,7 @@ const AchievementsBadges = () => (
                     </h3>
                 </div>
             </div>
-            <div className="rn-weAre-one color-shape-7">
+            {/* <div className="rn-weAre-one color-shape-7">
                 <div className="row g-5 d-flex justify-content-center">
                     <div className="col-lg-4 col-md-6 col-6">
                         <div className="rn-about-wrapper">
@@ -42,6 +42,38 @@ const AchievementsBadges = () => (
                                 />
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div> */}
+            <div className="d-flex flex-wrap gap-5 justify-content-center">
+                <div className="badge-card">
+                    <div className="inner-badge">
+                        <Image
+                            src="/images/achievements/2023-solver-team-seal.png"
+                            alt="2023-solver-team-seal"
+                            width={300}
+                            height={200}
+                        />
+                    </div>
+                </div>
+                <div className="badge-card">
+                    <div className="inner-badge">
+                        <Image
+                            src="/images/achievements/gsr.png"
+                            alt="gsr"
+                            width={300}
+                            height={200}
+                        />
+                    </div>
+                </div>
+                <div className="badge-card">
+                    <div className="inner-badge">
+                        <Image
+                            src="/images/achievements/community-award.jfif"
+                            alt="community-award"
+                            width={300}
+                            height={200}
+                        />
                     </div>
                 </div>
             </div>
