2a6e7644e969ee359e5ea7529a9a6a181ed4c136
ManiByanjankar <mani@rumsan.net>
2024-10-04 18:11:13 +0545

    upadtes

--- src/components/list/career/careerList.jsx
+++ src/components/list/career/careerList.jsx
@@ -8,16 +8,7 @@ const CareerList = ({ career, path = "/career" }) => {
         return null;
     }
     return (
-        <Link
-            href={`${path}/${career?.slug}`}
-            style={{
-                fontFamily: "Poppins",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "normal",
-                color: ` ${career?.status === false ? "red" : "#2B7EC1"}  `,
-            }}
-        >
+        <>
             <Container
                 className="my-5"
                 style={{
@@ -39,9 +30,11 @@ const CareerList = ({ career, path = "/career" }) => {
                                 fontStyle: "normal",
                                 fontWeight: 500,
                                 lineHeight: "normal",
+                                color: "#2B7EC1",
                             }}
                         >
-                            {career?.designation}
+                            Evaluation of Rahat System for Anticipatory Action
+                            Pilot Project (October 15, 2024)
                         </span>
                     </Col>
                     <Col sm={4} className="text-end">
@@ -57,14 +50,76 @@ const CareerList = ({ career, path = "/career" }) => {
                                 // }  `,
                             }}
                         >
-                            {career?.status === false ? "Closed" : "Apply Now"}
+                            <a
+                                href="https://assets.rumsan.net/rumsan-group/evaluation-of-rahat-system-for-anticipatory-action-pilot-project.pdf"
+                                target="_blank"
+                                style={{ color: "#2B7EC1" }}
+                            >
+                                Read More
+                            </a>
                         </span>
                     </Col>
                 </Row>
             </Container>
-        </Link>
+            <Link
+                href={`${path}/${career?.slug}`}
+                style={{
+                    fontFamily: "Poppins",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "normal",
+                    color: ` ${career?.status === false ? "red" : "#2B7EC1"}  `,
+                }}
+            >
+                <Container
+                    className="my-5"
+                    style={{
+                        borderBottom: "1px solid grey",
+                        cursor: "pointer",
+                    }}
+                >
+                    <Row className="my-4">
+                        <Col className="d-flex align-items-centers" sm={8}>
+                            <Iconify
+                                icon="ph:bag-bold"
+                                width={30}
+                                style={{ color: "#231F20" }}
+                            />
+                            <span
+                                className="fs-2 mx-4"
+                                style={{
+                                    fontFamily: "Poppins",
+                                    fontStyle: "normal",
+                                    fontWeight: 500,
+                                    lineHeight: "normal",
+                                }}
+                            >
+                                {career?.designation}
+                            </span>
+                        </Col>
+                        <Col sm={4} className="text-end">
+                            <span
+                                className="fs-4 mx-4 "
+                                style={{
+                                    fontFamily: "Poppins",
+                                    fontStyle: "normal",
+                                    fontWeight: 500,
+                                    lineHeight: "normal",
+                                    // color: ` ${
+                                    //     career?.status === false ? "red" : "#2B7EC1"
+                                    // }  `,
+                                }}
+                            >
+                                {career?.status === true
+                                    ? "Closed"
+                                    : "Apply Now"}
+                            </span>
+                        </Col>
+                    </Row>
+                </Container>
+            </Link>
+        </>
     );
 };
 
 export default CareerList;
-
