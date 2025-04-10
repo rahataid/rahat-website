ff1f82d70e8396cef88daacdd7a76d43680c9b34
Raktim Shrestha <rakimsth@gmail.com>
2025-04-10 15:09:49 +0545

    env file for ganalytics

--- src/components/google-analytics/index.jsx
+++ src/components/google-analytics/index.jsx
@@ -2,7 +2,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 const GoogleAnalyticsWrapper = () => {
     const googleId =
-        process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "G-ZE2FSW0Y79";
+        "G-ZE2FSW0Y79" || process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
     return (
         <>
             <GoogleAnalytics gaId={googleId} />
