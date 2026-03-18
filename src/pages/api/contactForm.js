9d9cb88f971b5b0100c95a7e7b9a90c6e623d149
Ashok Bhattarai <ashok.bhattarai@rumsan.net>
2026-03-18 11:48:49 +0545

    feat: Integrate Google reCAPTCHA v2 component in contact form

--- src/pages/api/contactForm.js
+++ src/pages/api/contactForm.js
@@ -2,14 +2,24 @@ import mailer from "../../utils/mailer";
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
-        // const { body } = req;
-        // console.log("body", body);
-        // const { payload, token } = JSON.parse(body);
-        // console.log("payload", payload);
-        // if (!payload || !token) throw new Error("Oops! Something went wrong.");
+        const { recaptchaToken, ...formData } = req.body; // 👈 extract token
+
+        //  verify with Google
+        const verifyRes = await fetch(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`,
+            { method: "POST"
+             }
+        );
+        const verifyData = await verifyRes.json();
+
+        if (!verifyData.success) {
+            return res.status(400).json({ error: "reCAPTCHA verification failed" });
+        }
+
+        // continue with your existing email/form logic using formData
         try {
             await mailer
-                .contactForm(req.body)
+                .contactForm(formData)
                 .then((d) => {
                     res.status(200).json(d);
                 })
